cdd1aa57a15075483b319e9c9f56a2d9f104e4aa
BDC <baptiste.do-cabo@etu.univ-lyon1.fr>
2025-03-25 16:40:08 +0100

    Add UDP port scanning functionality to Client class

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,34 @@
+import java.net.BindException;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Client {
+
+public static Map<Integer, Boolean> scanUDPPorts(int startPort, int endPort) {
+    Map<Integer, Boolean> portStatus = new HashMap<>();
     
+    for (int port = startPort; port <= endPort; port++) {
+        DatagramSocket socket = null;
+        try {
+            
+            socket = new DatagramSocket(port);
+            portStatus.put(port, false);
+            System.out.println("Port " + port + " is closed (available)");
+        } catch (BindException e) {
+            portStatus.put(port, true);
+            System.out.println("Port " + port + " is open (in use)");
+        } catch (SocketException e) {
+            System.err.println("Error checking port " + port + ": " + e.getMessage());
+        } finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }
+    }
+    
+    return portStatus;
+}
 }
