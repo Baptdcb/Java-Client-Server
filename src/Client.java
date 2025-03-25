957c8776a919846b3b1b3115fb05568bd04dcf52
Nath <nathan.girardi@proton.me>
2025-03-25 17:09:05 +0100

    Code client

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
 import java.net.BindException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +9,29 @@ import java.net.InetSocketAddress;
 
 
 public class Client {
+
+    public Client (InetSocketAddress adress, String message){
+        try {
+            // 1 - Création du canal
+            DatagramSocket socketClient = new DatagramSocket();
+            InetAddress adresseClient = InetAddress.getByName("localhost");
+            byte[] envoyees; // tampon d'émission
+            byte[] recues = new byte[1024]; // tampon de réception
+            // 2 - Émettre 
+            envoyees = message.getBytes();
+            DatagramPacket messageEnvoye = new DatagramPacket(envoyees, envoyees.length, adresseClient, 6666);
+            socketClient.send(messageEnvoye);
+            // 3 - Recevoir
+            DatagramPacket paquetRecu = new DatagramPacket(recues, recues.length);
+            socketClient.receive(paquetRecu);
+            String reponse = new String(paquetRecu.getData(), 0, paquetRecu.getLength());
+            System.out.println("Depuis le serveur: " + reponse);
+            // 4 - Libérer le canal
+            socketClient.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 
     public static Map<Integer, Boolean> scanUDPPorts(String ipAddress, int startPort, int endPort) {
         Map<Integer, Boolean> portStatus = new HashMap<>();
@@ -32,4 +57,6 @@ public class Client {
         
         return portStatus;
     }
+
+    
 }
