6bb6f6cc21c575bf282fda88aa616305480db1bd
BDC <baptiste.do-cabo@etu.univ-lyon1.fr>
2025-03-25 17:08:48 +0100

    Implement UDP server functionality to receive and respond to messages

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,33 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 public class Server {
+    public Server(InetSocketAddress address, String message){ 
     
+        try{
+            // 1 - Création du canal
+            DatagramSocket socketServeur = new DatagramSocket(null);
+            // 2 - Réservation du port
+
+            socketServeur.bind(address);
+            byte[] recues = new byte[1024]; // tampon d'émission
+            byte[] envoyees; // tampon de réception
+            // 3 - Recevoir
+            DatagramPacket paquetRecu = new DatagramPacket(recues, recues.length);
+            socketServeur.receive(paquetRecu);
+            System.out.println("Reçu: " + message);
+            // 4 - Émettre
+            InetAddress adrClient = paquetRecu.getAddress(); int prtClient = paquetRecu.getPort();
+            String reponse = "Accusé de réception"; envoyees = reponse.getBytes();
+            DatagramPacket paquetEnvoye = new DatagramPacket(envoyees, envoyees.length, adrClient, prtClient);
+            socketServeur.send(paquetEnvoye);
+            // 5 - Libérer le canal
+            socketServeur.close();
+        }catch(Exception e){
+            System.err.println(e);
+        }
+    
+    }
 }
