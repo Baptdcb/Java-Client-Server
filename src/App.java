0286c7a2c53a88cc00e2ddaef58ab12c23942fb9
BDC <baptiste.do-cabo@etu.univ-lyon1.fr>
2025-03-25 16:52:58 +0100

    Refactor UDP port scanning to accept IP address and update output messages

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // Scan ports 1 to 65535
+        Map<Integer, Boolean> portStatus = Client.scanUDPPorts("localhost", 5173, 5174);
+        System.out.println(portStatus);
     }
 }
--- src/Client.java
+++ src/Client.java
@@ -3,32 +3,33 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.HashMap;
 import java.util.Map;
+import java.net.InetSocketAddress;
 
 
 public class Client {
 
-public static Map<Integer, Boolean> scanUDPPorts(int startPort, int endPort) {
-    Map<Integer, Boolean> portStatus = new HashMap<>();
-    
-    for (int port = startPort; port <= endPort; port++) {
-        DatagramSocket socket = null;
-        try {
-            
-            socket = new DatagramSocket(port);
-            portStatus.put(port, false);
-            System.out.println("Port " + port + " is closed (available)");
-        } catch (BindException e) {
-            portStatus.put(port, true);
-            System.out.println("Port " + port + " is open (in use)");
-        } catch (SocketException e) {
-            System.err.println("Error checking port " + port + ": " + e.getMessage());
-        } finally {
-            if (socket != null && !socket.isClosed()) {
-                socket.close();
+    public static Map<Integer, Boolean> scanUDPPorts(String ipAddress, int startPort, int endPort) {
+        Map<Integer, Boolean> portStatus = new HashMap<>();
+        
+        for (int port = startPort; port <= endPort; port++) {
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket(null);
+                socket.bind(new InetSocketAddress(ipAddress, port));
+                portStatus.put(port, false);
+                System.out.println("Port " + port + " is closed (libre) on IP " + ipAddress);
+            } catch (BindException e) {
+                portStatus.put(port, true);
+                System.out.println("Port " + port + " is open (occupé) on IP " + ipAddress);
+            } catch (SocketException e) {
+                System.err.println("Error checking port " + port + " on IP " + ipAddress + ": " + e.getMessage());
+            } finally {
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
             }
         }
+        
+        return portStatus;
     }
-    
-    return portStatus;
-}
 }
