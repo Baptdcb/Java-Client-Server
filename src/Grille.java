21f17420710acba97677ff78e55254b6205138b1
Nath <nathan.girardi@proton.me>
2025-04-05 22:37:07 +0200

    Commenataires grille

--- src/Grille.java
+++ src/Grille.java
@@ -1,11 +1,22 @@
+
+/**
+ * Classe permettant de reproduire une grille de morpion et de pouvoir y jouer
+ */
 public class Grille {
 
+    /** Grille de morpion */
     public char[][] grille;
 
+    /**
+     * Constructeur, initialise la grille
+     */
     public Grille() {
         initialiserGrille();
     }
 
+    /**
+     * initialise chaque élément de la grille par le caractère espace
+     */
     public void initialiserGrille() {
         this.grille = new char[3][3];
         for (int i = 0; i < 3; i++) {
@@ -15,6 +26,14 @@ public class Grille {
         }
     }
 
+    /**
+     * Permet à un des joueurs d'effectuer un coup
+     * 
+     * @param numJoueur Le numéro du joueur (0 ou 1)
+     * @param ligne     La ligne du coup
+     * @param colonne   La colonne du coup
+     * @return Si le coup a été joué ou non
+     */
     public boolean jouer(int numJoueur, int ligne, int colonne) {
         if (grille[ligne][colonne] == ' ') {
             if (numJoueur == 0) {
@@ -27,6 +46,9 @@ public class Grille {
         return false;
     }
 
+    /**
+     * Affiche la grille dans la console
+     */
     public void afficherGrille() {
         String returnString = "";
         for (char[] ligne : grille) {
@@ -41,6 +63,9 @@ public class Grille {
         System.out.println(returnString);
     }
 
+    /**
+     * Retourne la chaîne de caractères permettant d'afficher la grille
+     */
     public String toString() {
         String returnString = "";
         for (char[] ligne : grille) {
@@ -55,6 +80,12 @@ public class Grille {
         return returnString;
     }
 
+    /**
+     * Vérifie si le symbole passé en entrée a gagné la partie
+     * 
+     * @param symbole Le symbole d'un des joueurs ('X' ou 'O')
+     * @return Booléen induquant si le joueur a gagné la partie
+     */
     public boolean verifierGagnant(char symbole) {
 
         for (int i = 0; i < 3; i++) {
@@ -80,6 +111,11 @@ public class Grille {
         return false;
     }
 
+    /**
+     * Vérifie si la grille est remplie
+     * 
+     * @return Booléen indiquand si la grille est remplie
+     */
     public boolean grilleRemplie() {
         for (int ligne = 0; ligne < 3; ligne++) {
             for (int colonne = 0; colonne < 3; colonne++) {
